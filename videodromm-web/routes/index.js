41ec9e38ae6cce8b85aed662d19693d1f3d0ea90
Bruce LANE <bruce@batchass.fr>
2016-10-25 23:39:44 +0200

    azure and localhost ok

--- videodromm-web/routes/index.js
+++ videodromm-web/routes/index.js
@@ -1,13 +1,14 @@
 ﻿var express = require('express');
 var router = express.Router();
-//const fs = require('fs');
+const fs = require('fs');
 
 router.get('/', function (req, res) {
-	res.send('respond with a resource');
-	//getHtmlFromFile(res);
+	
+	getHtmlFromFile(res);
 });
-/*function getHtmlFromFile(res) {
-	fs.readFile('./public/html/the_force.html', (err, data) => {
+function getHtmlFromFile(res) {
+	fs.readFile('./public/the_force.html', (err, data) => {
+	
 		if (err) return hadError(err, res);
 		const html = data.toString();
 		res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -17,6 +18,6 @@ router.get('/', function (req, res) {
 function hadError(err, res) {
 	console.error(err);
 	res.end('Server Error' + err);
-}*/
+}
 
 module.exports = router;
